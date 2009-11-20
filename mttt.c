890f28a0f0ef8f99302b2a216ba106c923ceee22
Andrew Gray <sweetandy@gmail.com>
2009-11-20 11:18:01 -0800

    actual board size commit

--- mttt.c
+++ mttt.c
@@ -9,36 +9,22 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <math.h>
 
 #define TRUE 1
 #define FALSE 0
 
 #define X 1
 #define O 2
+#define DRAW 3
 
-#define A board[0][0]
-#define B board[0][1]
-#define C board[0][2]
-#define D board[1][0]
-#define E board[1][1]
-#define F board[1][2]
-#define G board[2][0]
-#define H board[2][1]
-#define I board[2][2]
+/* The following defines board size currently; subject to change */
+#define SIZE 3
 
-/* Now that's what I call making code compact. Unfortunately, this is a
- * horrible hack, and is blatant abuse of #define. I do believe a rewrite is
- * in order, my good friend.
- */
-
-#define PRINT case FALSE : printf(" "); break;
-#define SPOT case X : printf("X"); break;
-#define OWNER case O : printf("O");
- 
 /* Function declarations */
 
 void clearboard();
-void checkvictory();
+int det_winner();           // Returns FALSE, X, O, or DRAW values
 void endgame(int);
 void getinput();
 int checkinput(int, int);   // Returns TRUE if input is valid
@@ -46,11 +32,12 @@ void printboard();
 
 /* Global variable declarations */
 
-int board[3][3];
-int i, ii;                 // Used in 'for' loops
+int board[SIZE][SIZE];     // The gameplay board
+int i, ii;                 // Used in for loops as counters
 int getoutnow = FALSE;     // Bool, TRUE signifies the end of a game
-int i_row, i_col;          // input stuff
+int i_row, i_col;          // Input stuff
 int currentplayer = 0;     // X for X, O for O (1 and 2 respectively)
+int x, y;                  // For loops w/ board[][]
 
 /* Begin function definitions */
 
@@ -58,55 +45,74 @@ void clearboard()
 {
     // Clear array board[][]; fill it with zeros
 
-    for (i = 0; i < 3; i++)
-        {
-            for (ii = 0; ii < 3; ii++)
-                {
-                    board[i][ii] = 0;
-                }
-        }
+    for (i = 0; i < SIZE; i++)
+        for (ii = 0; ii < SIZE; ii++)
+            board[i][ii] = 0;
 }
 
-void checkvictory()
+int det_winner()
 {
-    // Check for X's victory
+    // X/O horizontal
+    for (x = 0; x < SIZE; x++)
+    {
+        i = ii = 0;
 
-    if ((A * B * C == 1) ||
-        (D * E * F == 1) ||
-        (G * H * I == 1) ||
-        (A * D * G == 1) ||
-        (B * E * H == 1) ||
-        (C * F * I == 1) ||
-        (A * E * I == 1) ||
-        (C * E * G == 1))
-            endgame(X);
+        for (y = 0; y < SIZE; y++)
+        {
+            if (board[x][y] == X) i++;
+            if (board[x][y] == O) ii++;
+        }
 
-    // Check for O's victory
+        if (i == SIZE) return X;
+        if (ii == SIZE) return O;
+    }
 
-    if ((A * B * C == 8) ||
-        (D * E * F == 8) ||
-        (G * H * I == 8) ||
-        (A * D * G == 8) ||
-        (B * E * H == 8) ||
-        (C * F * I == 8) ||
-        (A * E * I == 8) ||
-        (C * E * G == 8))
-            endgame(O);
+    // X/O vertical
+    for (y = 0; y < SIZE; y++)
+    {
+        i = ii = 0;
 
-    // Check for tie; only if winner has not yet been found
+        for (x = 0; x < SIZE; x++)
+        {
+            if (board[x][y] == X) i++;
+            if (board[x][y] == O) ii++;
+        }
 
-   if (((A != 0) &&
-        (B != 0) &&
-        (C != 0) &&
-        (D != 0) &&
-        (E != 0) &&
-        (F != 0) &&
-        (G != 0) &&
-        (H != 0) &&
-        (I != 0)) &&
-       (getoutnow == FALSE))    // Makes sure that a winner has not been found yet
-            endgame(FALSE);
+        if (i == SIZE) return X;
+        if (ii == SIZE) return O;
+    }
 
+    // Check X/O diagonals
+
+    for (x = 0, y = 0, i = 0, ii = 0; x < SIZE; x++, y++)
+    {
+        if (board[x][y] == X) i++;
+        if (board[x][y] == O) ii++;
+    }
+
+    if (i == SIZE) return X;
+    if (ii == SIZE) return O;
+
+    for (x = 0, y = SIZE - 1, i = 0, ii = 0; x < SIZE; x++, y--)
+    {
+        if (board[x][y] == X) i++;
+        if (board[x][y] == O) ii++;
+    }
+
+    if (i == SIZE) return X;
+    if (ii == SIZE) return O;
+
+    // Check for tie
+
+    for (x = 0, i = 0; x < SIZE; x++)
+        for (y = 0; y < SIZE; y++)
+            if (board[x][y] > 0)
+                i++;
+
+    if (i == SIZE * SIZE)
+        return DRAW; 
+
+    return FALSE;
 }
 
 void endgame(int winner)
@@ -114,42 +120,46 @@ void endgame(int winner)
     getoutnow = TRUE; // Used to tell other function calls not to run
 
     if (winner == X)
-    {
         puts("X has won!");
-    }
 
     if (winner == O)
-    {
         puts("O has won!");
-    }
 
-    if (winner == FALSE)
-    {
+    if (winner == DRAW)
         puts("Cat has won!");
-    }
 }
 
 void printboard()
 {
-    printf("\n1| ");
-    switch (A) { PRINT SPOT OWNER }
-    printf(" | ");
-    switch (B) { PRINT SPOT OWNER }
-    printf(" | ");
-    switch (C) { PRINT SPOT OWNER }
-    printf("\n |---+---+---\n2| ");
-    switch (D) { PRINT SPOT OWNER }
-    printf(" | ");
-    switch (E) { PRINT SPOT OWNER }
-    printf(" | ");
-    switch (F) { PRINT SPOT OWNER }
-    printf("\n |---+---+---\n3| ");
-    switch (G) { PRINT SPOT OWNER }
-    printf(" | ");
-    switch (H) { PRINT SPOT OWNER }
-    printf(" | ");
-    switch (I) { PRINT SPOT OWNER }
-    printf("\n +-----------\n   1   2   3\n\n");
+    printf("\n");
+    
+    for (x = 0; x < SIZE; x++)
+    {
+        for (y = 0; y < SIZE; y++)
+        {
+            switch (board[x][y])
+            {
+                case FALSE : printf("|%d,%d", x + 1, y + 1);
+                    break;
+                case X : printf("| X ");
+                    break;
+                case O : printf("| O ");
+            }
+        }
+        
+        printf("\n|");
+
+        for (i = 0; i < SIZE; i++)
+        {
+            printf("---");
+            if (i != SIZE - 1)
+                printf("+");
+            if (i == SIZE - 1)
+                printf("\n");
+        }
+    }
+
+    printf("\n");
 }
 
 void getinput()
@@ -162,14 +172,10 @@ void getinput()
                 break;
             case O : puts("It's O's turn!");
         }
-/*
-        printf("Please input row [1-3]: ");
-        scanf("%d", &i_row);
-        printf("Please input col [1-3]: ");
-        scanf("%d", &i_col);
-*/
+        
+        i_row = i_col = 0;
 
-        printf("Please input row,col [1-3],[1-3]: ");
+        printf("Please input row,col [1-%d],[1-%d]: ", SIZE, SIZE);
         scanf("%d,%d", &i_row, &i_col);
 
         if (checkinput(i_row, i_col) == TRUE)
@@ -181,48 +187,40 @@ void getinput()
 
 int checkinput(int row, int col)
 {
-    if (((1 <= row) && (row <= 3)) &&       // These two lines ensure
-        ((1 <= col) && (col <= 3)))         // the input is within range
+    if (((1 <= row) && (row <= SIZE)) &&       // These two lines ensure
+        ((1 <= col) && (col <= SIZE)))         // the input is within range
     {
-    
         if (board[row-1][col-1] == 0)
         {
             board[row-1][col-1] = currentplayer;
             return TRUE;
         }
         else
-        {
             return FALSE;
-        }
     }
     else
-    {
         return FALSE;
-    }
 }
 
 int main()
 {
-
     currentplayer = X;
 
     while (TRUE)
     {
         // New Game!
-        // Clear the board
     
         clearboard();
 
         while (getoutnow == FALSE)
         {
-
             printboard();
-            checkvictory();
+
+            if (det_winner() > FALSE)
+                endgame(det_winner());
             
             if (getoutnow == FALSE)
                 getinput();
-
-            // Switch player
 
             switch (currentplayer)
             {
@@ -235,9 +233,7 @@ int main()
         getoutnow = FALSE;      // Reset to allow another game
 
         // Would you like to play again?
-        // 1. Yes -- do nothing
-        // 2. No -- exit();
-
+        
         break;
     }
 
